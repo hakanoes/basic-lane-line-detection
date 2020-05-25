cf207095841232faec71f5aee78b6fcd94187b20
Hakan Ören <hakanorenceo@gmail.com>
2020-05-25 03:18:02 +0300

    healty codes

--- lane_line_detection.py
+++ lane_line_detection.py
@@ -1,62 +1,37 @@
 import cv2
 import numpy as np
 
-cap = cv2.VideoCapture("video.mp4",0)
+cap = cv2.VideoCapture("vi.mp4")
+kernel = np.ones((5,5),np.uint8)
 
-while True:
+while cap.isOpened():
     ret, frame = cap.read()
-    # resize video
-    resize = frame[350:800,300:1500]
-    # convert to hls
+    resize = frame[300:800,300:1500]
     blur = cv2.GaussianBlur(resize, (5,5), 0)
     hls = cv2.cvtColor(blur, cv2.COLOR_BGR2HLS)
-    ret, th = cv2.threshold(hls, 100, 255, cv2.THRESH_BINARY)
+    ret, th = cv2.threshold(hls, 127, 255, cv2.THRESH_BINARY)
+        
+    lower = np.array([0,102,51])
+    upper = np.array([255,255,255])
 
-    # white masked
-    lower_white = np.array([146,146,146])
-    upper_white = np.array([255,255,255])
-    white_mask = cv2.inRange(hls, lower_white, upper_white)
-
-    # yellow masked
-    lower_yellow = np.array([10, 0, 100])
-    upper_yellow = np.array([40, 255, 255])
-    yellow_mask = cv2.inRange(hls,  lower_yellow, upper_yellow)
-
-    mask = cv2.bitwise_or(white_mask, yellow_mask)
-    masked = cv2.bitwise_and(white_mask, yellow_mask)
-    edges = cv2.Canny(masked, 75, 150)
+    mask = cv2.inRange(th, lower, upper)
+    edges = cv2.Canny(mask, 75, 150)
+    edges = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 0, maxLineGap=100)
     
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 30, maxLineGap=50)
-
-
     if lines is not None:
+
         for line in lines:
-            x, y ,w ,h = line[0]
-            cv2.line(blur, (x, y), (w, h), (0,255,0), 3)
-
-
-
-    cv2.imshow("mask",masked)
-    cv2.imshow("blur",th)
-
-
-    key = cv2.waitKey(25)
-    if key == 27:
+            x, y, w, h = line[0]
+            cv2.line(blur, (x,y),(w,h), (0,255,0), 1)
+            
+    
+    cv2.imshow("hls",hls)
+    cv2.imshow("th",mask)
+    cv2.imshow("test",blur)
+    key = cv2.waitKey(30)
+    if key == ord('q'):
         break
-
 cap.release()
-cv2.destroyAllWindows()
-
-
-
-
-
-
-
-
-
-
-
-
-
+cv2.destroyAllWindows
 
